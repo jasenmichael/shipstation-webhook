297d3369222e4ce571398888eef0b059814e5fac
jasenmichael <jasen@jasenmichael.com>
2022-11-07 21:48:12 -0600

    chore: revert

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const compression = require('compression')
 
 const isServerless = process.env.SERVERLESS
 
-const webhooks = require('./webhooks')
+const { shipstation } = require('./webhooks')
 
 const app = express()
 app.use(compression())
@@ -17,11 +17,13 @@ router.get('/webhooks', (req, res) => res.status(200).json({ message: 'ok' }))
 
 // Route to receive the ShipStation New Order Webhook
 router.post('/webhooks/shipstation/on-new-orders', async (req, res) => {
-  console.log(JSON.stringify(req.headers, null, 2)) // eslint-disable-line no-console
-  console.log(JSON.stringify(req.body, null, 2)) // eslint-disable-line no-console
-  // process.exit(0)
-  const response = await webhooks.shipstation.onNewOrders(req, res)
-  res.status(response?.error ? 500 : 200).json(response)
+  if (req.query.token !== process.env.SHIPSTATION_WEBHOOK_TOKEN) {
+    console.log(`🚫 ERROR - Shipstation webhook token does not match. Token: ${req.query.token}`) // eslint-disable-line
+    return res.status(401).json('Unauthorized')
+  } else {
+    const response = await shipstation.onNewOrders(req, res)
+    return res.status(200).json(response)
+  }
 })
 
 if (isServerless) {
